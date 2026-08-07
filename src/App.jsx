5b9805557ca01df2eb693c78c94274b61a246391
Shangmin <shangminch@gmail.com>
2026-08-06 20:09:59 -0400

    fix: add MotionConfig reducedMotion, remove dead scroll listener, fix mobile hover

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './styles/index.css';
 import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router';
+import { MotionConfig } from 'framer-motion';
 import { ThemeProvider } from './contexts/ThemeContext';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -11,13 +12,10 @@ import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Gallery from './components/Gallery';
 import Contact from './components/Contact';
-import { useScrollReveal } from './hooks/useScrollReveal';
 
 import GalleryPage from './components/GalleryPage';
 
 function App() {
-  useScrollReveal();
-
   const HomePage = () => (
     <>
       <Hero />
@@ -32,17 +30,19 @@ function App() {
 
   return (
     <ThemeProvider>
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/gallery" element={<GalleryPage />} />
-            <Route path="/gallery/:albumId" element={<GalleryPage />} />
-          </Routes>
-          <Footer />
-        </div>
-      </Router>
+      <MotionConfig reducedMotion="user">
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/gallery" element={<GalleryPage />} />
+              <Route path="/gallery/:albumId" element={<GalleryPage />} />
+            </Routes>
+            <Footer />
+          </div>
+        </Router>
+      </MotionConfig>
     </ThemeProvider>
   );
 }
